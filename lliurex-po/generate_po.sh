30d12b169ead16a1070191f404f46a353b002b29
juanma1980 <juanma1980@gmail.com>
2021-07-06 11:30:01 +0200

    Include all python files

--- lliurex-po/generate_po.sh
+++ lliurex-po/generate_po.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./generate_categories_po.py
+#./generate_categories_po.py
 ./generate_cli_errors_po.py
 
 PYTHON_FILES="../lliurex-store-gui/usr/share/lliurex-store/lliurex-store-gui/*.py categories.po"
@@ -13,6 +13,6 @@ xgettext $UI_FILES $PYTHON_FILES -o lliurex-store/lliurex-store.pot
 #CLI Files
 mkdir -p python3-lliurex-store/
 PYTHON_FILES="../lliurex-store-cli/usr/share/lliurex-store/*.py categories.po errors.po"
-LIB_FILES="../python3-lliurex-store.install/usr/share/lliurexstore/plugins/loadStore.py"
+LIB_FILES="../python3-lliurex-store.install/usr/share/lliurexstore/plugins/*.py"
 xgettext $LIB_FILES $PYTHON_FILES -o python3-lliurex-store/python3-lliurex-store.pot
 
